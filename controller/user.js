07e7373ef099307ce7c5e85f49bc314817abe63e
mayank20011 <mayank.behl.mb1@gmail.com>
2025-07-26 15:02:28 +0530

    controller user.js password reset functions changed i think

--- controller/user.js
+++ controller/user.js
@@ -122,7 +122,6 @@ export const loginUser = async (req, res) => {
 
 export const sendOtp = async (req, res) => {
   const { email } = req.body;
-  console.log(req.body);
   if (!email) {
     return res.status(400).json({
       success: false,
